2371d86928a028187544b12f1a7296b51b219cd4
josh <jhollow6@asu.edu>
2018-04-03 15:48:06 -0700

    Modified Linear PCA with an Auto-Encoder

--- Deep_Learning_Research/readme.txt
+++ Deep_Learning_Research/readme.txt
@@ -1,4 +1,19 @@
 In /tensorflow/udemy/udemy_tf01.py linear regression is performed with tensorflow.
 In /tensorflow/udemy/udemy_tf01.py linear PCA is performed with an auto-encoder.
 
+Note: The fully_connected tf function makes it difficult to access the paramaters
+to use custom loss function with regularization with frobenius norms. I created
+a function that replaces the fully_connected function with a standard matrix multiply,
+but it only works on the first layer.
 
+
+Need to fix this second layer, then:
+-Introduce sparsity parameter
+-Train on MNIST with arch: 784-200-784
+-Extract the encoder
+-Add new layer to encoder with softmax classifier => arch: 784-200-10
+-Train while holding W1,b1 constant.
+-Train a new network with random parameters with architecture 784-200-10
+-Test each of these networks and compare the network using the encoder with the network that didn't use a pre-trained first layer.
+
+Shoule use the MNIST code and mod it to an auto-encoder
--- Deep_Learning_Research/tensorflow/udemy/auto_encoder_mod_of_mnist_example.py
+++ Deep_Learning_Research/tensorflow/udemy/auto_encoder_mod_of_mnist_example.py
@@ -1,16 +1,8 @@
 # Linear Auto-encoder
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
 
-def layer(input, num_inputs, num_neurons, method):
-    if method == 0:
-        layer_name = fully_connected(input,num_neurons,activation_fn=None) # Linear act. func
-    else:
-        W = tf.Variable(tf.zeros([num_inputs, num_neurons])) # Num Inputs x Num Neurons
-        b = tf.Variable(tf.zeros([num_neurons]))
-        layer_name = tf.matmul(input, W) + b
-    return layer_name
+from sklearn.datasets import make_blobs
 
 # Create 100 samples (100 rows of data)
 # Each example has 3 features (3 columns)
@@ -19,8 +11,8 @@ data = make_blobs(n_samples=100, n_features=3, centers=2, random_state=101)
 # data is a tuple containing
 # data[0] contains the data matrix
 # data[1] contains the labels
-#X = data[0]
-#Y = data[1]
+X = data[0]
+Y = data[1]
 
 # pre-process data
 from sklearn.preprocessing import MinMaxScaler
@@ -49,27 +41,48 @@ num_outputs = num_inputs
 
 learning_rate = 0.01
 
-X = tf.placeholder(tf.float32,shape=[None,num_inputs]) # how is the data passed into thic column vector?
-hidden = layer(input=X, num_inputs=num_inputs, num_neurons=num_hidden, method=1)
-outputs = fully_connected(hidden,num_outputs,activation_fn=None) # Linear act. func
-#outputs = layer(input=hidden, num_inputs=num_hidden, num_neurons=num_outputs, method=1)
 
-# What dataype is outputs?
-print('outputs is a '+str(type(outputs)))
+X = tf.placeholder(tf.float32,shape=[None,num_inputs])
+#hidden = fully_connected(X,num_hidden,activation_fn=None) # Linear act. func
+#outputs = fully_connected(hidden,num_outputs,activation_fn=None) # Linear act. func
 
-loss = tf.reduce_mean(tf.square(outputs-X))
+
+
+#==========
+# Layer 1:
+W1 = tf.Variable(tf.zeros([num_inputs, num_hidden])) # Num Inputs x Num Neurons
+b1 = tf.Variable(tf.zeros([num_hidden]))
+y1 = tf.matmul(X, W1) + b1
+
+# Layer 2:
+W2 = tf.Variable(tf.zeros([num_hidden, num_outputs])) # Num Inputs x Num Neurons
+b2 = tf.Variable(tf.zeros([num_outputs]))
+y2 = tf.matmul(y1, W2) + b2
+#==========
+
+
+
+loss = tf.reduce_mean(tf.square(y2-X))
 optimizer = tf.train.AdamOptimizer(learning_rate)
 train = optimizer.minimize(loss)
 
 # initialize variables
 init = tf.global_variables_initializer()
 
-num_steps = 1000
+#num_steps = 1000
+#with tf.Session() as sess:
+#    sess.run(init)
+#    for iteration in range(num_steps):
+#        sess.run(train,feed_dict={X:scaled_data}) # the data is automatically vectorized?
+#    output_2d = hidden.eval(feed_dict={X:scaled_data})
+
+# Train the net
 with tf.Session() as sess:
     sess.run(init)
-    for iteration in range(num_steps):
-        sess.run(train,feed_dict={X:scaled_data}) # the data is automatically vectorized?
-    output_2d = hidden.eval(feed_dict={X:scaled_data})
+    for _ in range(1000):
+        # Execute y2 = W2*(W1*x + b1) + b2
+        sess.run(train, feed_dict={X: scaled_data})
+    output_2d = y1.eval(feed_dict={X:scaled_data})
 
 # Loop at the reduced dimensioned data
 #plt.scatter(output_2d[:,0],output_2d[:,1])
