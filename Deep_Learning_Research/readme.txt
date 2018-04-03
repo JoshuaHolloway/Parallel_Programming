eb8b56d4b009fe156b89be11e848b55bf36c7bc4
josh <jhollow6@asu.edu>
2018-04-03 13:22:01 -0700

    linear PCA with an auto-encoder added

---
+++ Deep_Learning_Research/readme.txt
@@ -0,0 +1,4 @@
+In /tensorflow/udemy/udemy_tf01.py linear regression is performed with tensorflow.
+In /tensorflow/udemy/udemy_tf01.py linear PCA is performed with an auto-encoder.
+
+
---
+++ Deep_Learning_Research/tensorflow/udemy/udemy_tf01.py
@@ -0,0 +1,68 @@
+# Linear Auto-encoder
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_blobs
+
+# Create 100 samples (100 rows of data)
+# Each example has 3 features (3 columns)
+# Two classes
+data = make_blobs(n_samples=100, n_features=3, centers=2, random_state=101)
+# data is a tuple containing
+# data[0] contains the data matrix
+# data[1] contains the labels
+X = data[0]
+Y = data[1]
+
+# pre-process data
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(data[0]) # normalize dataset
+
+# Plot data
+data_x = scaled_data[:,0] # 1st feature
+data_y = scaled_data[:,1] # 2nd feature
+data_z = scaled_data[:,2] # 3rd feature
+
+# Look at the data
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+ax.scatter(data_x, data_y, data_z,c=data[1])
+
+
+# Perform PCA with Tensorflow
+import tensorflow as tf
+from tensorflow.contrib.layers import fully_connected
+
+num_inputs = 3
+num_hidden = 2
+num_outputs = num_inputs
+
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32,shape=[None,num_inputs]) # how is the data passed into thic column vector?
+hidden = fully_connected(X,num_hidden,activation_fn=None) # Linear act. func
+outputs = fully_connected(hidden,num_outputs,activation_fn=None) # Linear act. func
+
+# What dataype is outputs?
+print('outputs is a '+str(type(outputs)))
+
+loss = tf.reduce_mean(tf.square(outputs-X))
+optimizer = tf.train.AdamOptimizer(learning_rate)
+train = optimizer.minimize(loss)
+
+# initialize variables
+init = tf.global_variables_initializer()
+
+num_steps = 1000
+with tf.Session() as sess:
+    sess.run(init)
+    for iteration in range(num_steps):
+        sess.run(train,feed_dict={X:scaled_data}) # the data is automatically vectorized?
+    output_2d = hidden.eval(feed_dict={X:scaled_data})
+
+# Loop at the reduced dimensioned data
+#plt.scatter(output_2d[:,0],output_2d[:,1])
+ax.scatter(output_2d[:,0],output_2d[:,1], np.zeros([100,]), data_z,c=data[1])
+plt.show()
